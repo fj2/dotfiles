3b8cb6573a2d825ab3512f5e22717107bfcda6d5
Finn Johnson <finn.johnson15@imperial.ac.uk>
2017-07-26 13:53:17 +0100

    Added echos to install script to make it clearer

--- install.sh
+++ install.sh
@@ -10,27 +10,36 @@ else
   exit
 fi
 
+echo
+
 # Remove the trailing '/'s from directories
 for program in ${programs///}; do
   echo "Restoring files for ${program}"
   stow "${program}"
 done
 
+echo
+
 echo "Installing vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+echo
+
 echo "You may wish to install:"
+echo " - feh"
 echo " - i3"
 echo " - i3lock"
-echo " - polybar"
-echo " - pulseaudio"
-echo " - feh"
-echo " - unclutter"
-echo " - redshift"
 echo " - light"
 echo " - maim"
-echo " - xclip"
+echo " - polybar"
+echo " - pulseaudio"
+echo " - redshift"
 echo " - rofi"
+echo " - unclutter"
+echo " - xclip"
+echo " - xfce4-terminal"
+
 echo
+
 echo "Setup complete"
